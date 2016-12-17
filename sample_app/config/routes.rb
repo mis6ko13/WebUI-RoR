542bc575c398ff35b7d1626a4068691210d6d6e0
mis6ko13 <mis6ko13@gmail.com>
2016-12-17 21:25:12 +0200

    chapter 12 finished

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
@@ -14,5 +18,6 @@ Rails.application.routes.draw do
 
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 
 end
